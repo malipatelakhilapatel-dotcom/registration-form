f3175cdf864117c3c2ec8e0de114fb062a282cd1
malipatelakhilapatel-dotcom <malipatelakhilapatel@gmail.com>
2025-12-17 15:19:23 +0530

    Add files via upload

---
+++ user_validation_streamlit.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import sqlite3
+
+conn=sqlite3.connect("userdb.db",check_same_thread=False)
+cursor=conn.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS users(name text PRIMARY KEY,password text)""")
+
+choice=st.sidebar.selectbox("MENU",["REGISTER","LOGIN"])
+if choice=="REGISTER":
+    name=st.text_input("USERNAME")
+    password=st.text_input("PASSWORD",type="password")
+    if st.button("REGISTER"):
+        cursor.execute("""INSERT INTO users(name,password)VALUES(?,?)""",(name,password))
+        conn.commit()
+        st.balloons()
+if choice=="LOGIN":
+    name=st.text_input("USERNAME")
+    password=st.text_input("PASSWORD",type="password")
+    if st.button("LOGIN"):
+        cursor.execute("""SELECT*FROM users WHERE
+        name=? AND password=?""",(name,password))
+        result=cursor.fetchone()
+        if result:
+            st.success("valid user")
+            st.snow()
+        else:
+            st.error("invalid")
